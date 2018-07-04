160b56fd7423b3ff558ad5877bb0da914948e49a
Tom <thomashollis1@gmail.com>
2018-07-04 23:51:26 +0100

    v1.0

---
+++ java/16_JavaSubstringComparisons.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        for (int i = 0; i < (s.length() - k + 1); i++) {
+            String substring = s.substring(i,  i + k);
+            if (substring.compareTo(smallest) < 0 || smallest.isEmpty()) {
+                smallest = substring;
+            }
+            if (substring.compareTo(largest) > 0 || largest.isEmpty()) {
+                largest = substring;
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
