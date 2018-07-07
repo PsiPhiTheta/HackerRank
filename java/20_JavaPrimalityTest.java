d6e50d8f01ce02198ee77027177f3ad7e1d24f30
Tom <thomashollis1@gmail.com>
2018-07-07 12:14:43 +0100

    v1.0

---
+++ java/20_JavaPrimalityTest.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+
+        scanner.close();
+        
+        BigInteger numBig = new BigInteger(n);
+        
+        if (numBig.isProbablePrime(5)) {
+              System.out.println("prime");
+        } 
+        else
+        {
+              System.out.println("not prime");
+        }
+    }
+}
