d11733915a9a1cf74b8c7df81d745ef101c3341d
Tom <thomashollis1@gmail.com>
2018-07-11 18:35:22 +0100

    v1.1

--- java/27_JavaBitset.java
+++ java/27_JavaBitset.java
@@ -6,37 +6,38 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+	int a = sc.nextInt();
+	int b = sc.nextInt();
 
-		BitSet[] BS = new BitSet[3];
-		for (int i = 1; i < BS.length; i++) {
-			BS[i] = new BitSet(a);
-		}
-		for (int i = 0; i < b; i++) {
-			String operation = sc.next();
-			int input1 = sc.nextInt();
-			int input2 = sc.nextInt();
-            
-			if (operation.equals("AND")) {
-				BS[input1].and(BS[input2]);
-			} 
-            else if (operation.equals("OR")) {
-				BS[input1].or(BS[input2]);
-			} 
-            else if (operation.equals("XOR")) {
-				BS[input1].xor(BS[input2]);
-			} 
-            else if (operation.equals("FLIP")) {
-				BS[input1].flip(input2);
-			} 
-            else if (operation.equals("SET")) {
-				BS[input1].set(input2);
-			}
+	BitSet[] BS = new BitSet[3];
+	for (int i = 1; i < BS.length; i++) {
+		BS[i] = new BitSet(a);
+	}
+	    
+	for (int i = 0; i < b; i++) {
+		String operation = sc.next();
+		int input1 = sc.nextInt();
+		int input2 = sc.nextInt();
 
-			System.out.println(BS[1].cardinality() + " " + BS[2].cardinality());
+		if (operation.equals("AND")) {
+			BS[input1].and(BS[input2]);
+		} 
+		else if (operation.equals("OR")) {
+			BS[input1].or(BS[input2]);
+		} 
+		else if (operation.equals("XOR")) {
+			BS[input1].xor(BS[input2]);
+		} 
+		else if (operation.equals("FLIP")) {
+			BS[input1].flip(input2);
+		} 
+		else if (operation.equals("SET")) {
+			BS[input1].set(input2);
 		}
 
-		sc.close();
+		System.out.println(BS[1].cardinality() + " " + BS[2].cardinality());
+	}
+
+	sc.close();
     }
 }
