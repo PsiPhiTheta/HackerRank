a3db2bf60ee231657cd40d2b47ad3873c9a65bd7
Tom <thomashollis1@gmail.com>
2018-08-13 15:01:39 +0100

    v1.0

---
+++ PS/SecurityFunctions.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the calculate function below.
+    static int calculate(int x) {
+        int xr = x%11;
+        return xr;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int x = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = calculate(x);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
