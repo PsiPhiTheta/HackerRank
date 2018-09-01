ad857610e526b800eb579d5d6354d0ab1c79903a
Tom <thomashollis1@gmail.com>
2018-09-01 01:21:30 -0400

    v1.0

---
+++ PS/InsertionSortPart1.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the insertionSort1 function below.
+    static void insertionSort1(int n, int[] arr) {
+        int temp = arr[n-1];
+        for (int i = n-1; i>=0; i--) {
+            if (i == 0) {
+                arr[0] = temp;
+                i = -1;
+            }
+            else {
+                arr[i] = arr[i-1];
+                if (temp > arr[i]) {
+                    arr[i] = temp;
+                    i = -1;
+                }
+            }
+            for (int j = 0; j<arr.length; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        insertionSort1(n, arr);
+
+        scanner.close();
+    }
+}
