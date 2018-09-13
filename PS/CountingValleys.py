4a7bc2e3a02d2282dd323c154fecd733cf6f94b6
Tom <thomashollis1@gmail.com>
2018-09-13 01:20:53 -0400

    v1.1

---
+++ PS/CountingValleys.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    altitude = 0
+    valleys = 0
+    for i, char in enumerate(s):
+        if (char == "D"):
+            altitude -= 1
+        else:
+            altitude += 1
+            if altitude == 0:
+                valleys += 1
+
+    return valleys
+        
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
