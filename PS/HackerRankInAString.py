86f35a1ae37f478c0dbd35cdc98f090cd70de1b7
Tom <thomashollis1@gmail.com>
2018-12-26 01:13:46 +0100

    v1.0

---
+++ PS/HackerRankInAString.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hackerrankInString function below.
+def hackerrankInString(s):
+    sub_string = "hackerrank"
+    idx = 0
+    
+    for char in s:
+        if char == sub_string[idx]:
+            idx+=1
+            if idx == len(sub_string):
+                return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = hackerrankInString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
