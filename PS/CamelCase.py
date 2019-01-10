16e38988acfbb8cd4ed96c563411693d51866137
Tom <thomashollis1@gmail.com>
2019-01-09 22:04:18 -0500

    v1.1

---
+++ PS/CamelCase.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the camelcase function below.
+def camelcase(s):
+    out = 1
+    for char in s:
+        if char.isupper():
+            out += 1
+    return out
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
