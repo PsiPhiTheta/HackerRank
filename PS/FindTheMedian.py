9ce3b145a5a24cc19d82fdd60284d5020db251e2
Tom <thomashollis1@gmail.com>
2019-01-13 00:05:22 -0500

    v2.1

--- PS/FindTheMedian.py
+++ PS/FindTheMedian.py
@@ -1,12 +1,9 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-# Complete the findMedian function below.
 def findMedian(arr):
     arr.sort()
     mid = int(len(arr)/2)
