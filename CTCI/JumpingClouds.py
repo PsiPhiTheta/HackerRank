a9e5214e98de47db3762ac513cfd33a70fe953e1
Tom <thomashollis1@gmail.com>
2019-01-20 23:22:02 -0500

    v1.1

---
+++ CTCI/JumpingClouds.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    count = 0
+    i = 0
+    dist = len(c)
+    while i < dist-1:
+        if (i+2 < dist and c[i+2] == 0):
+            count += 1
+            i += 2
+        else:
+            count += 1
+            i += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
