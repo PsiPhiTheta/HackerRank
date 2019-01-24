8badf38f0dfd96df8677ad65b193be22d5933c8f
Tom <thomashollis1@gmail.com>
2019-01-24 10:16:29 -0500

    v2.1

--- CTCI/RepeatedString.py
+++ CTCI/RepeatedString.py
@@ -7,9 +7,8 @@ import sys
 def repeatedString(s, n):
     count1 = s.count("a")
     count2 = s[0:(n%len(s))].count("a")
-    total = (n//len(s))*count1 + count2
-    return total
-
+    return ((n//len(s))*count1 + count2)
+  
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
