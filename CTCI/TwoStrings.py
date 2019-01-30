9a3ea14312e89e9d5d3cc8bc227cb493d553f2bd
Tom <thomashollis1@gmail.com>
2019-01-30 00:02:40 -0500

    v2.0

---
+++ CTCI/TwoStrings.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    lst1 = list(s1)
+    lst2 = list(s2)
+    for i in lst1:
+        if i in s2:
+            return "YES"
+    for j in lst2:
+        if j in s1:
+            return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
